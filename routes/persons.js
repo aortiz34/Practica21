d0e696a29f46fdb0b01c403f369f872ea7934638
Axel Efraín Ortiz Reyes <aortiz@MacBook-Pro-de-Axel.local>
2022-05-24 19:59:39 -0500

    practica21

--- routes/persons.js
+++ routes/persons.js
@@ -7,7 +7,8 @@ let Person = require('../models/person');
 router.get('/persons', function (req, res, next) {
     Person.find(function(err, persons) {
         if (err) return next(err);
-        res.json(persons);
+        //res.json(persons);
+        res.render('personsIndex',{persons})
     });
 });
 
